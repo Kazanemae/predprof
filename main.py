236fd9a68e53e840ac06ccb3f9ba7b3063d07041
Mertysh <100412812+Mertysh@users.noreply.github.com>
2024-03-31 12:43:38 +0300

    back

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ from calcul import formula
 
 
 data = get_date_info(25, 1, 23)
-print(data)
 rooms = formula(data['rooms_count'], data['windows_for_room_list'],{
     "floor_1": data['windows_dict']['floor_1'],
     "floor_2": data['windows_dict']['floor_2'],
     "floor_3": data['windows_dict']['floor_3'],
     "floor_4": data['windows_dict']['floor_4']
 })
-
 print(post_answer(rooms, '25-01-23'))
 
 room_dict = {}
@@ -22,5 +20,3 @@ for floor in range(1, 5):
         for __ in range(windows_for_room):
             room_list.append(room)
     room_dict[f'floor_{floor}'] = room_list
-
-print(room_dict)
