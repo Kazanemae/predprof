1f2f52b3ba98eb32e68e0d7e75e9f9a65dd80128
Vitaz-Vitaz <76260006+Vitaz-Vitaz@users.noreply.github.com>
2024-03-31 14:16:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from api import get_date, get_date_info, post_answer
 from calcul import formula
+from flask import Flask, render_template, jsonify
+from sqlite3 import connect
+
+import connection as connection
+from requests import get, post
+
+TOKEN = 'ppo_9_30000'
+HEADERS = {'X-Auth-Token': TOKEN}
+
+app = Flask(__name__)
 
 
 def get_all_dates():
@@ -8,13 +18,13 @@ def get_all_dates():
     return date_list
 
 
-def get_info(date: str): #dd-mm-yy
+def get_info(date: str):  # dd-mm-yy
     day = date.split('-')[0]
     month = date.split('-')[1]
     year = date.split('-')[2]
 
     data = get_date_info(day, month, year)
-    rooms = formula(data['flats_count'], data['windows_for_flat'],{
+    rooms = formula(data['flats_count'], data['windows_for_flat'], {
         "floor_1": data['windows_dict']['floor_1'],
         "floor_2": data['windows_dict']['floor_2'],
         "floor_3": data['windows_dict']['floor_3'],
@@ -35,11 +45,70 @@ def get_info(date: str): #dd-mm-yy
     data_to_return = {
         'window_list': window_list,
         'true_rooms': rooms,
-        'true_flats_count': len(rooms),
-        'flats_count': data['flats_count'],
+        'true_rooms_count': len(rooms),
+        'rooms_count': data['flats_count'],
         'windows': data['windows_for_flat'],
         'server_answer': server_answer,
         'date': date
     }
-
+    print(data_to_return)
     return data_to_return
+
+
+def getTT(day, month, year):
+    req = get(f'https://olimp.miet.ru/ppo_it_final?day={day}&month={month}&year={year}', headers=HEADERS)
+    data = req.json()['message']
+    return data
+
+
+stroka = '25-01-23'
+day = stroka.split('-')[0]
+month = stroka.split('-')[1]
+year = stroka.split('-')[2]
+data = getTT(day, month, year)
+s = get_info(stroka)["window_list"]
+s1 = get_info(stroka)["true_rooms"]
+comnaty = get_info(stroka)["true_rooms_count"]
+result = get_info(stroka)["server_answer"]
+
+
+def insertINtoBD(ans):
+    n = len(ans["windows"]["data"])
+    # print('|'.join(ans["windows"]["data"]))
+    dataForWindows = []
+    for i in range(n):
+        dataForWindows.append('|'.join(ans["windows"]["data"]["floor_" + str(i + 1)]))
+    s1 = '!'.join(dataForWindows)
+    s2 = list(map(str, ans["windows_for_room"]["data"]))
+    windows_for_rooms = '|'.join(s2)
+    cursor = connection.cursor()
+    id = len(cursor.execute("SELECT * FROM res").fetchall())
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(
+            """INSERT INTO res (id, data, roomscount, zapolnenieRooms, dataForWindows)
+            VALUES  (?, ?, ?, ?, ?)""",
+            (id, ans["date"]["data"], ans["rooms_count"]["data"], windows_for_rooms, s1))
+        connection.commit()
+    print(cursor.execute("SELECT * FROM res").fetchall())
+
+
+data = getTT(day, month, year)
+insertINtoBD(data)
+print(data)
+
+
+@app.route('/')
+def index():
+    wList = s
+    nomera = s1
+    return render_template('index2.html', rooms_count=data['flats_count']['data'],
+                           windows_for_room=data['windows_for_flat']['data'],
+                           windows=data['windows']['data'], n=len(data["windows"]["data"]), mas=wList,
+                           lenMas=len(wList), kolvo=len(wList[0]), comnaty=comnaty, nomera=nomera, result=result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
+
+# print(get_info('25-01-23'))
