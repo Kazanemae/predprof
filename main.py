a5400bb90f3eae290351a68477205d43f3a279a3
Vitaz-Vitaz <76260006+Vitaz-Vitaz@users.noreply.github.com>
2024-03-31 12:59:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,31 @@
-from api import get_date, get_date_info, post_answer
-from calcul import formula
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
 
 
-def get_all_dates():
-    date_list = get_date()
-
-    return date_list
-
-
-def get_info(date: str): #dd-mm-yy
-    day = date.split('-')[0]
-    month = date.split('-')[1]
-    year = date.split('-')[2]
-
-    data = get_date_info(day, month, year)
-    rooms = formula(data['rooms_count'], data['windows_for_room_list'],{
-        "floor_1": data['windows_dict']['floor_1'],
-        "floor_2": data['windows_dict']['floor_2'],
-        "floor_3": data['windows_dict']['floor_3'],
-        "floor_4": data['windows_dict']['floor_4']
-    })
-    server_answer = post_answer(rooms, date)
-
-    window_list = []
-    room = 0
-    for _ in range(1, 5):
-        room_list = []
-        for windows_for_room in data['windows_for_room_list']:
-            room += 1
-            for __ in range(windows_for_room):
-                room_list.append(room)
-        window_list.append(room_list)
-
-    data_to_return = {
-        'window_list': window_list,
-        'true_rooms': rooms,
-        'true_rooms_count': len(rooms),
-        'rooms_count': data['rooms_count'],
-        'windows': data['windows_for_room_list'],
-        'server_answer': server_answer,
-        'date': date
+@app.route('/')
+def index():
+    data = {
+        "date": {"data": 1674594000, "description": "Татьянин день"},
+        "rooms_count": {"data": 3, "description": "Количество комнат на этаже"},
+        "windows_for_room": {"data": [3, 2, 1],
+                             "description": "Количество окон в каждой из комнат на этаже слева направо"},
+        "windows": {
+            "data": {
+                "floor_1": [False, True, False, True, False, False],
+                "floor_2": [True, True, True, False, False, True],
+                "floor_3": [False, False, True, False, True, False],
+                "floor_4": [False, False, False, True, False, True]
+            },
+            "description": "Окна по этажам, в которых горит свет"
+        }
     }
 
-    return data_to_return
+    return render_template('index2.html', rooms_count=data['rooms_count']['data'],
+                           windows_for_room=data['windows_for_room']['data'],
+                           windows=data['windows']['data'], n=len(data["windows"]["data"]), mas=[[1, 1, 1, 2, 2, 3], [4, 4, 4, 5, 5, 6], [7, 7, 7, 8, 8, 9], [10, 10, 10, 11, 11, 12]],
+                           lenMas=len([[1, 1, 1, 2, 2, 3], [4, 4, 4, 5, 5, 6], [7, 7, 7, 8, 8, 9], [10, 10, 10, 11, 11, 12]]))
 
 
-print(get_info('25-01-23'))
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
