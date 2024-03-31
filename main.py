b0b78fcbe15e00848c1809e6ac3cfb2e18ea8852
Mertysh <100412812+Mertysh@users.noreply.github.com>
2024-03-31 12:56:29 +0300

    Добил main

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def get_info(date: str): #dd-mm-yy
 
     window_list = []
     room = 0
-    for floor in range(1, 5):
+    for _ in range(1, 5):
         room_list = []
         for windows_for_room in data['windows_for_room_list']:
             room += 1
@@ -32,15 +32,17 @@ def get_info(date: str): #dd-mm-yy
                 room_list.append(room)
         window_list.append(room_list)
 
-    return window_list
-    # data_to_return = {
-    #     'true_rooms': rooms,
-    #     'true_rooms_count': rooms,
-    #     'window_list': window_list,
+    data_to_return = {
+        'window_list': window_list,
+        'true_rooms': rooms,
+        'true_rooms_count': len(rooms),
+        'rooms_count': data['rooms_count'],
+        'windows': data['windows_for_room_list'],
+        'server_answer': server_answer,
+        'date': date
+    }
 
-
-    # }
+    return data_to_return
 
 
 print(get_info('25-01-23'))
-# [[1, 1, 1, 2, 2, 3], [4, 4, 4, 5, 5, 6], [7, 7, 7, 8, 8, 9], [10, 10, 10, 11, 11, 12]]
